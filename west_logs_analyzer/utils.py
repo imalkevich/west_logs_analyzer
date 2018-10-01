ff95c245c4ffc3872cbd6a6a4a6890837b98df51
Ihar Malkevich <Ihar_Malkevich@epam.com>
2018-10-01 09:06:50 -0500

    adjust for weekends

--- west_logs_analyzer/utils.py
+++ west_logs_analyzer/utils.py
@@ -3,10 +3,18 @@ import sys
 from datetime import datetime, timedelta
 
 def get_days_range(interval):
-    end = datetime.now() + timedelta(days=-1)
-    start = end + timedelta(days=-1*interval+1)
-    span = end - start
-    days_range = [(start + timedelta(days=i)).date() for i in range(span.days + 1)]
+    def subtract_day(day):
+        return day + timedelta(days=-1)
+
+    days_range = []
+    day_to_add = datetime.today() + timedelta(days=-1)
+    while len(days_range) < interval:
+        if day_to_add.weekday() == 5 or day_to_add.weekday() == 6:
+            day_to_add = subtract_day(day_to_add)
+        else:
+            days_range.insert(0, day_to_add)
+            day_to_add = subtract_day(day_to_add)
+
     return days_range
 
 TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S %Z'
